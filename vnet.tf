8dde44b73b76f9b3f5768d50faaad9ca7f2dc45f
fad <fadupup69@outlook.com>
2020-10-14 13:58:53 +0000

    Added vnet moduel v4

--- vnet.tf
+++ vnet.tf
@@ -1,10 +1,14 @@
 module "network" {
-  source  = "app.terraform.io/mftech-fad/network/azurerm"
+  source              = "app.terraform.io/mftech-fad/network/azurerm"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
   version = "3.0.1"
-  # insert required variables here
+  address_space       = "10.0.0.0/16"
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
 
-  variable "resource_group_name"{
-    Description = "The name of an existing resource group to be imported."
+  tags = {
+    environment = "dev"
+    costcenter  = "it"
   }
 
 }
