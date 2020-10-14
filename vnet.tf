5e22eedff69fb45f7538b1a4148651e8da85468a
fad <fadupup69@outlook.com>
2020-10-14 13:36:16 +0000

    Added vnet modulev2

--- vnet.tf
+++ vnet.tf
@@ -2,4 +2,8 @@ module "network" {
   source  = "app.terraform.io/mftech-fad/network/azurerm"
   version = "3.2.1"
   # insert required variables here
+  variable "resource_group_name"{
+    Description = "The name of an existing resource group to be imported."
+  }
+
 }
